d8a88ed33544bbf12eab65a1811f23792cffd5d8
Sadburritos <Pochtagleba@bk.ru>
2023-05-19 21:50:37 +0600

    Update var.py

--- may19/var.py
+++ may19/var.py
@@ -1,2 +1,8 @@
-print(2+2)
-print("Yo")
+userint1 = int(input("enter nubmer 1 "))
+userint2 = int(input("enter nubmer 2 "))
+
+result1 = userint1 + userint2
+
+result2 = result1 // 2
+
+print (result2)
